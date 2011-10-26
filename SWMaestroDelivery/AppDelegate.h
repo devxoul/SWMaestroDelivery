86006a89439bb5d328991d31e56dd44977ff5301
Su-Yeol Jeon <devxoul@gmail.com>
2011-10-26 12:17:19 +0900

    Initial Commit

---
+++ SWMaestroDelivery/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SWMaestroDelivery
+//
+//  Created by 전 수열 on 11. 10. 26..
+//  Copyright (c) 2011년 Joyfl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SWMaestroDelivery/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  SWMaestroDelivery
+//
+//  Created by 전 수열 on 11. 10. 26..
+//  Copyright (c) 2011년 Joyfl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+	}
+}
